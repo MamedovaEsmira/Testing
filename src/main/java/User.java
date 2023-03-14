a2573989fbf060e58009f2a41d6a3fb05b95dcbc
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-03-14 16:10:07 +0300

    дз сделано

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,34 +1,23 @@
 public class User {
-    private String login;
-    private String email;
+    private final String  login;
+    private final String email;
 
     public User(String login, String email) {
-        this.login = login;
-        this.email = email;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        if (login == null || login.isEmpty()) {
+        if (login == null ) {
             throw new IllegalArgumentException("Поле логин пустое!");
         } else {
             this.login = login;
         }
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        if (login == null || email.isEmpty()) {
+        if (email == null ) {
             throw new IllegalArgumentException("Поле email пустое!");
         } else {
             this.email = email;
         }
     }
+    public String getLogin() {
+        return login;
+    }
+    public String getEmail() {
+        return email;
+    }
 }
-
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,36 +1,31 @@
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.regex.Pattern;
 
 class UserTest {
     private final static String LOGIN = "Alex";
     private final static String EMAIL = "alex@gmail.com";
     private final static String INCORRECT_EMAIL = "alex@mailru";
     private final static String MATCHER_MAIL = ("\\w+@\\w+\\.\\w+");
-    private static User userTest;
 
-    @BeforeEach
-    void createUser(){
-        userTest = new User(LOGIN, EMAIL);
+    @Test
+    void shouldCorrectUser() {
+        User user= new User(LOGIN, EMAIL);
+        Assertions.assertFalse(user.getLogin().isEmpty());
     }
 
     @Test
-    void shouldCorrectSetLogin() {
-        userTest.setLogin(LOGIN);
-        Assertions.assertEquals(LOGIN, userTest.getLogin());
+    void shouldIncorrectUser() {
+        User user= new User("","");
+        Assertions.assertTrue(user.getLogin().isEmpty());
     }
 
     @Test
-    void shouldCorrectSetEmail() {
-        userTest.setEmail(EMAIL);
-        Assertions.assertEquals(EMAIL, userTest.getEmail());
-    }
+    void checkingUserEmail(){
+        User user= new User(LOGIN, EMAIL);
+        User incorrectUser= new User(LOGIN,INCORRECT_EMAIL);
 
-    @Test
-    void shouldNotVerifyIncorrectEmail(){
-        userTest.setEmail(INCORRECT_EMAIL);
-        Assertions.assertFalse(Pattern.matches(MATCHER_MAIL, userTest.getEmail()));
+        Assertions.assertTrue(user.getEmail().matches(MATCHER_MAIL));
+        Assertions.assertFalse(incorrectUser.getEmail().matches(MATCHER_MAIL));
     }
 }
